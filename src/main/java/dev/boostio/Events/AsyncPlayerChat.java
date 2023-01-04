97d88b6a1a4af8229ab5ce38a53ddfde6231a4eb
Boostio <70571116+BoostioAaron@users.noreply.github.com>
2023-01-04 20:52:19 +0100

    Fixed the frozen chat

--- src/main/java/dev/boostio/Events/AsyncPlayerChat.java
+++ src/main/java/dev/boostio/Events/AsyncPlayerChat.java
@@ -2,6 +2,7 @@ package dev.boostio.Events;
 
 import dev.boostio.ModFunctionality;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,7 +15,11 @@ public class AsyncPlayerChat implements Listener {
         Player player = event.getPlayer();
         boolean isFrozen = ModFunctionality.getInstance().getPlayerData().get(player.getUniqueId()).isFrozen();
         if(isFrozen){
-            Bukkit.broadcast(event.getMessage(), "modfunctionality.freeze.use");
+            String Message = event.getMessage();
+            event.setCancelled(true);
+            String frozenMessage = "§7(§4§l§nFROZEN§7) §c" + player.getDisplayName() + ": §f" + Message;
+            Bukkit.broadcast(frozenMessage, "modfunctionality.freeze.use");
+            player.sendMessage(frozenMessage);
         }
     }}
 }
